2c1897d6d5b4301b4b4384ee0bfe588bf2fa810e
Mathieu Lorber <mathieu.lorber@gmail.com>
2022-04-14 11:01:02 +0200

    build

---
+++ CMakeFiles/pgquarrel.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/mlo/git/lite-pgquarrel/src/aggregate.c" "CMakeFiles/pgquarrel.dir/src/aggregate.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/aggregate.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/am.c" "CMakeFiles/pgquarrel.dir/src/am.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/am.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/cast.c" "CMakeFiles/pgquarrel.dir/src/cast.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/cast.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/collation.c" "CMakeFiles/pgquarrel.dir/src/collation.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/collation.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/common.c" "CMakeFiles/pgquarrel.dir/src/common.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/common.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/conversion.c" "CMakeFiles/pgquarrel.dir/src/conversion.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/conversion.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/domain.c" "CMakeFiles/pgquarrel.dir/src/domain.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/domain.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/eventtrigger.c" "CMakeFiles/pgquarrel.dir/src/eventtrigger.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/eventtrigger.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/extension.c" "CMakeFiles/pgquarrel.dir/src/extension.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/extension.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/fdw.c" "CMakeFiles/pgquarrel.dir/src/fdw.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/fdw.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/function.c" "CMakeFiles/pgquarrel.dir/src/function.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/function.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/index.c" "CMakeFiles/pgquarrel.dir/src/index.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/index.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/language.c" "CMakeFiles/pgquarrel.dir/src/language.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/language.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/matview.c" "CMakeFiles/pgquarrel.dir/src/matview.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/matview.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/operator.c" "CMakeFiles/pgquarrel.dir/src/operator.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/operator.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/policy.c" "CMakeFiles/pgquarrel.dir/src/policy.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/policy.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/privileges.c" "CMakeFiles/pgquarrel.dir/src/privileges.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/privileges.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/publication.c" "CMakeFiles/pgquarrel.dir/src/publication.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/publication.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/quarrel.c" "CMakeFiles/pgquarrel.dir/src/quarrel.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/quarrel.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/rule.c" "CMakeFiles/pgquarrel.dir/src/rule.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/rule.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/schema.c" "CMakeFiles/pgquarrel.dir/src/schema.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/schema.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/sequence.c" "CMakeFiles/pgquarrel.dir/src/sequence.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/sequence.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/server.c" "CMakeFiles/pgquarrel.dir/src/server.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/server.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/statistics.c" "CMakeFiles/pgquarrel.dir/src/statistics.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/statistics.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/subscription.c" "CMakeFiles/pgquarrel.dir/src/subscription.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/subscription.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/table.c" "CMakeFiles/pgquarrel.dir/src/table.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/table.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/textsearch.c" "CMakeFiles/pgquarrel.dir/src/textsearch.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/textsearch.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/transform.c" "CMakeFiles/pgquarrel.dir/src/transform.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/transform.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/trigger.c" "CMakeFiles/pgquarrel.dir/src/trigger.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/trigger.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/type.c" "CMakeFiles/pgquarrel.dir/src/type.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/type.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/usermapping.c" "CMakeFiles/pgquarrel.dir/src/usermapping.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/usermapping.c.o.d"
+  "/Users/mlo/git/lite-pgquarrel/src/view.c" "CMakeFiles/pgquarrel.dir/src/view.c.o" "gcc" "CMakeFiles/pgquarrel.dir/src/view.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/mlo/git/lite-pgquarrel/mini/CMakeFiles/mini.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
